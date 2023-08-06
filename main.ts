55f01a532d2f760033f2c8aca830a2a7e71c3ec1
cheangkokfook <141533007+cheangkokfook@users.noreply.github.com>
2023-08-06 10:41:56 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -22,7 +22,7 @@ export function resetPID () {
     previousError = 0
 }
 
-//% block="computePID setPoint measureValue"
+//% block="computePID $setPoint $measureValue"
 export function computePID (setPoint: number, measuredValue: number) {
     error = setPoint - measuredValue
     integral += error
