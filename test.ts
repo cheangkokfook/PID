60fe5e0447f51ad59f85fed069a8f6f47a1060a5
cheangkokfook <141533007+cheangkokfook@users.noreply.github.com>
2023-08-07 08:44:11 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,13 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+let output = 0
+PID.setPIDConstants(1, 0.1, 0.01)
+let setpoint = 0
+let measuredValue = 10
+basic.forever(function () {
+    basic.showNumber(output)
+    basic.pause(500)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+    output = PID.computePID(setpoint, measuredValue)
+    measuredValue += -3
+})
